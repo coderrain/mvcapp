f445844ab9e3d04c16c53412775a49e39a88f357
coderrain <413842590@qq.com>
2019-04-12 16:01:44 +0800

    添加控制器

--- admin.js
+++ admin.js
@@ -14,59 +14,58 @@ app.engine('html', swig.renderFile);
 
 app.use('/', express.static('./public'));
 
-
-/*app.get('/', function(req, res, next){
-	res.send('hello word');
-})*/
-
+var data = [{
+	title: '小说',
+	id: 1,
+	des: '佛挡杀佛解释了发送的佛挡',
+	url: 'http://f.hiphotos.baidu.com/image/h%3D300/sign=f888027cebdde711f8d245f697eecef4/71cf3bc79f3df8dcfcea3de8c311728b461028f7.jpg'
+},{
+	title: '修真',
+	id: 2,
+	des: '修真修真修真修真修真',
+	url: 'http://g.hiphotos.baidu.com/image/h%3D300/sign=1ae574b8e8cd7b89f66c3c833f254291/1e30e924b899a90182ea2f4413950a7b0308f5e1.jpg'
+}]
 app.get('/', function(req, res, next){
-	res.render('a', {
-		title: '我的首页'
-	});
-})
-
-
-app.get('/search', function(req, res, next){
-	console.log(req.query.sa);
-	var result = null;
-	var data = [{
-		name: 'iphone',
-		price: 100
-	},{
-		name: 'sanxing',
-		price: 1001
-	},{
-		name: 'nuojiya',
-		price: 10011
-	},{
-		name: 'jinli',
-		price: 100111
-	}]
-	for(var i=0;i<data.length;i++){
-		if(req.query.sa == data[i].name){
-			result = data[i];
-			break;
-		}
-	}
-	console.log(result);
-	res.render('search', {
-		title: '搜索页面',
-		result: result
-	});
-
-
-})
-
-
-
-app.get('/list', function(req, res, next){
-	res.render('list',{
-		code: 1,
-		data: [{name:'iphone',price:100},{name:'sanxing',price:100},{name:'nuojiya',price:100}]
+	res.render('index',{
+		title: '首页',
+		data: [{
+			title: '小说',
+			id: 1,
+			des: '佛挡杀佛解释了发送的佛挡'
+		},{
+			title: '修真',
+			id: 2,
+			des: '修真修真修真修真修真'
+		}]
 	})
 })
 
+app.get('/list3', function(req, res, next){
+	res.render('list3',{
+		data: data
+	})
+})
 
+//文章页
+app.get('/art', function(req, res, next){
+	var queryId = req.query.id;
+	var result = {};
+	for(var i=0;i<data.length;i++){
+		if(queryId == data[i].id){
+			result = data[i];
+		}
+	}
+	res.render('art',{
+		result: result
+	})
+})
+
+app.get('/about', function(req, res, next){
+	res.render('about',{
+		title: '关于我们',
+		data: '发送的艰苦奋斗舒服舒服的时刻发送的艰苦奋斗舒服舒服的时刻发送的艰苦奋斗舒服舒服的时刻发送的艰苦奋斗舒服舒服的时刻发送的艰苦奋斗舒服舒服的时刻'
+	})
+})
 
 app.listen(8080, function(){
 	console.log('服务已经开启端口是8080')
